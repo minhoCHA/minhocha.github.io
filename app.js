1481fe0e1a1c19eb717c47efbf5a15ea2569f9df
minhoCha <minhocha1004@gmail.com>
2020-07-02 11:12:17 -0700

    wipe panel implemented

--- app.js
+++ app.js
@@ -286,72 +286,110 @@ $(window).scroll(function () {
 });
 
 // build scene
-var scene_feature_pic = new ScrollMagic.Scene({ 
-  triggerElement: ".feature-pics-div-p", 
-  duration: 300 
+var scene_feature_pic = new ScrollMagic.Scene({
+  triggerElement: ".feature-pics-div-p",
+  duration: 500,
+  triggerHook: 0.5,
+  offset: 500
 })
   // animate color and top border in relation to scroll position
-  .setTween(".feature-pics-div-img-1", { 
-    scale: 1.2
+  .setTween(".feature-pics-div-img-1", {
+    scale: 2
   }) // the tween durtion can be omitted and defaults to 1
   .addIndicators({ name: "feature pic" }) // add indicators (requires plugin)
-  .setPin('.feature-pics-div-p-1-1')
+  .setPin('.feature-pics-div-img')
   .addTo(controller);
 
-  var scene_feature_pic_p = new ScrollMagic.Scene({
-    triggerElement: ".feature-pics-div-img-1",
-    triggerHook: 0.9, // show, when scrolled 10% into view
-    duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-    offset: 50 // move trigger to center of element
-  })
+var scene_feature_pic_p = new ScrollMagic.Scene({
+  triggerElement: ".feature-pics-div-img-1",
+  triggerHook: 0.3, // show, when scrolled 10% into view
+  duration: "0%", // hide 10% before exiting view (80% + 10% from bottom)
+  offset: 50 // move trigger to center of element
+})
   .setClassToggle(".feature-pics-div-img-div1", "visible") // add class to reveal
-  .addIndicators({ name: "feature text"}) // add indicators (requires plugin)
+  .addIndicators({ name: "feature text" }) // add indicators (requires plugin)
   .addTo(controller);
 
-  $(function () { // wait for document ready
-		// init
-		var controller = new ScrollMagic.Controller({
-			globalSceneOptions: {
-				triggerHook: '.feature-pics-div-img-div3',
-				duration: "100%" // this works just fine with duration 0 as well
-				// However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
-				// Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
-			}
-		});
 
-		// get all slides
-		var slides = document.querySelectorAll("section.panel");
 
-		// create scene for every slide
-		for (var i=0; i < slides.length; i++) {
-			new ScrollMagic.Scene({
-					triggerElement: slides[i]
-				})
-				.setPin(slides[i], {pushFollowers: false})
-				.addIndicators({ name: "slides scene"}) // add indicators (requires plugin)
-				.addTo(controller);
-		}
-  });
-  
-  var scene_feature_slide_p = new ScrollMagic.Scene({
-    triggerElement: ".feature-pics-div-img-1",
-    triggerHook: 0.1, // show, when scrolled 10% into view
-    duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-    offset: 30 // move trigger to center of element
-  })
-  .setClassToggle(".ffeature-pics-div-img-div4", "visible") // add class to reveal
-  .addIndicators({ name: "slides text"}) // add indicators (requires plugin)
-  .addTo(controller);
+// var scene_feature_slide_p = new ScrollMagic.Scene({
+//   triggerElement: ".feature-pics-div-img-1",
+//   triggerHook: 0.1, // show, when scrolled 10% into view
+//   duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+//   offset: 30 // move trigger to center of element
+// })
+//   .setClassToggle(".feature-pics-div-img-div4", "visible") // add class to reveal
+//   .addIndicators({ name: "slides text" }) // add indicators (requires plugin)
+//   .addTo(controller);
 
 // build scene
-var scene_feature_pic2 = new ScrollMagic.Scene({ 
-  triggerElement: ".feature-pics-div-p", 
+var scene_feature_pic2 = new ScrollMagic.Scene({
+  triggerElement: ".feature-pics-div-p",
   duration: 100
 })
   // animate color and top border in relation to scroll position
-  .setTween(".feature-pics-div-img-2", { 
-    scale: 0.7
+  .setTween(".feature-pics-div-img-2", {
+    scale: 2
   }) // the tween durtion can be omitted and defaults to 1
   .addIndicators({ name: "feature pic2" }) // add indicators (requires plugin)
   .setPin('.feature-pics-div-img-2')
   .addTo(controller);
+
+
+
+// var wipes_controller = new ScrollMagic.Controller({
+//   globalSceneOptions: {
+//     triggerHook: 0.2,
+//     duration: "100%" // this works just fine with duration 0 as well
+//     // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
+//     // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
+//   }
+// });
+
+// // get all slides
+// var wipes_slides = document.querySelectorAll("section.panel");
+
+// // create scene for every slide
+// for (var i = 0; i < wipes_slides.length; i++) {
+//   new ScrollMagic.Scene({
+//     triggerElement: wipes_slides[i],
+//     triggerHook: 0.5
+//   })
+//     .setPin(wipes_slides[i], { pushFollowers: false })
+//     .addIndicators({ name: "slides" }) // add indicators (requires plugin)
+//     .addTo(wipes_controller);
+// }
+
+// // build scene
+// var scene_small_pic2 = new ScrollMagic.Scene({
+//   triggerElement: ".panel img3",
+//   duration: 100,
+// })
+//   // animate color and top border in relation to scroll position
+//   .setTween(".feature-pics-div-img-2", {
+//     scale: 0.5
+//   }) // the tween durtion can be omitted and defaults to 1
+//   .addIndicators({ name: "small pic2" }) // add indicators (requires plugin)
+//   .setPin('.feature-pics-div-img-2')
+//   .addTo(controller);
+
+  let wipeController = new ScrollMagic.Controller({
+      globalSceneOptions: {
+          triggerHook: 'onLeave'
+      }
+  });
+
+  var wipes_slides = document.querySelectorAll("section.panel");
+
+  for(let panel of wipes_slides) {
+
+      var wipeScene = new ScrollMagic.Scene({
+          triggerElement: panel
+      })
+      .addIndicators()
+      .on('progress', function (e) {
+          console.log(e.progress.toFixed(3));
+      })
+      .setPin(panel) 
+      .addTo(wipeController);        
+  }
